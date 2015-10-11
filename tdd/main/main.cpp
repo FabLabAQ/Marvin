81c8e61b4e2fd740924c9f952aa43c976de14501
Tomassino Ferrauto <t_ferrauto@yahoo.it>
2015-10-11 10:29:40 +0200

    More work on the TDD version

---
+++ tdd/main/main.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * SequencerGUI                                                               *
+ * Copyright (C) 2015                                                         *
+ * Tomassino Ferrauto <t_ferrauto@yahoo.it>                                   *
+ * Luca Anastasio <anastasio.lu@gmail.com>                                    *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software                *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA *
+ ******************************************************************************/
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QtQml>
+#include "sequencer.h"
+#include "sequence.h"
+#include "serialcommunication.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	return app.exec();
+}
